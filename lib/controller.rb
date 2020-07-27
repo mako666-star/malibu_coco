3e982d075e146f2486e0ac451d309907b15e7dc7
fauvel <gorm.t@orange.fr>
2020-07-27 18:41:55 +0200

    firsty

---
+++ lib/controller.rb
@@ -0,0 +1,21 @@
+require_relative 'gossip'
+
+class ApplicationController < Sinatra::Base
+    get '/' do 
+        erb :index, locals: {gossips: Gossip.all}
+    end
+    get '/gossips/new/' do
+        erb :new_gossip
+    end
+    
+    get '/gossips/:id/' do 
+        erb :show, locals: {gossips: Gossip.find(:id)}
+    end
+
+    post '/gossips/new/' do 
+        Gossip.new(params["gossip_author"], params["gossip_content"]).save
+        redirect '/'
+    end
+
+end
+
